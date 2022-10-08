7d921cd87c7af313028d05a37e8d06c9181e657c
Prince <princenzu51@gmail.com>
2022-10-08 09:53:26 +0100

    Created a model

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
+
+
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=150)
+    date_released = models.DateTimeField()
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=500)
+    song_id = models.OneToOneField(Song, on_delete=models.CASCADE)
+
 # Create your models here.
